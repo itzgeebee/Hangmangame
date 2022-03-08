079f81527b0e0c60d5f1c8ac3add95127b62c788
Gideon <balogungideon12@gmail.com>
2022-03-08 23:40:25 +0100

    amazon price low price alert

---
+++ Amazon_price_scraper/main.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import smtplib
+
+amazon_site = "https://www.amazon.com/ASUS-Display-R9-5900HX-Keyboard-G513QR-ES96/dp/B08SJTW9LK/ref=sr_1_3?keywords=asus+rog&qid=1646175658&sr=8-3"
+headers = {"Accept-Language": "en-US,en;q=0.9",
+           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+           "Accept-Encoding":"gzip, deflate",
+           "Accept":"text/html,application/xhtml+xml,"
+                    "application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+           }
+
+response = requests.get(url=amazon_site, headers=headers).text
+
+soup = BeautifulSoup(response, "lxml")
+output = soup.find(name="span", class_="a-offscreen").get_text()
+new_output = float(output.replace("$","").replace(",",""))
+print(new_output)
+
+my_email = ""
+password = ""
+email = ""
+if new_output <= 1800:
+
+    with smtplib.SMTP("smtp.gmail.com") as new_connection:
+        new_connection.starttls()
+        new_connection.login(user=my_email, password=password)
+        new_connection.sendmail(from_addr=my_email, to_addrs=email,
+                                msg=f"Subject: Low price alert!. \n\n price for ASUS rog is below {new_output}")
+
